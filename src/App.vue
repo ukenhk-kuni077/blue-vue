9a8bee344dcdf4f514f874fd716529f95b409eec
amanoese <amanoese@gmail.com>
2016-10-01 01:59:15 +0900

    first commit

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+    <card class="sticky-action"> <!-- remove sticky-action class for normal action -->
+      <img slot="img" src="http://materializecss.com/images/office.jpg"></img>
+      <span slot="title">Title (click me)</span>
+      <p>Some Content</p>
+      <span slot="revealTitle">Title (click me)</span> <!-- required for reveal to work -->
+      <p slot="reveal">Significantly more Content</p>
+      <a slot="action" href="#card">Some sticky action</a>
+    </card>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      // note: changing this line won't causes changes
+      // with hot-reload because the reloaded component
+      // preserves its current state and we are modifying
+      // its initial state.
+      msg: 'Hello Vue!'
+    }
+  },
+  components:{
+    'card': require('vue-materialize/card')
+  }
+
+}
+</script>
+
+<style>
+body {
+  font-family: Helvetica, sans-serif;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+import Vue from 'vue'
+import App from './App.vue'
+import './materialize.config.scss'
+
+new Vue({
+  el: 'body',
+  components: { App }
+})
