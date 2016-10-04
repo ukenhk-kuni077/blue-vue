e7536db91ecadeff4b2e233cbc789bf357b02ed5
Seito Taka <seito.taka@jal.com>
2016-10-04 12:49:14 +0900

    component out app.vue

---
+++ src/Task.vue
@@ -0,0 +1,29 @@
+<template>
+  <collapsible>
+    <collapsible-item v-for="delete in task_deletes" :delte="delete" :index="$index">
+      <h4 slot="header">
+        <span @click="delete = !delete;e.preventDefault();">
+          <input type="checkbox" value="" v-model.sync="delete"></input>
+          <label></label>
+        </span>
+        Task1
+      </h4>
+      <p>body 1</p>
+    </collapsible-item>
+  </collapsiSble>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      task_deletes : [false,false,false]
+    }
+  },
+  components:{
+    'collapsible' : require('vue-materialize/collapsible'),
+    'collapsible-item' : require('vue-materialize/collapsible-item')
+  }
+
+}
+</script>
