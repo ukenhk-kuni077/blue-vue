cd15fb6e8911da64e3ffc119ecf88a1a56747680
amanoese <amanoese@gmail.com>
2016-10-22 17:46:09 +0900

    page routing for vue-router

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+
 import App from './App.vue'
+import Map from './Map.vue'
 import './materialize.config.scss'
 
-new Vue({
-  el: 'body',
-  components: { App }
-})
+Vue.use(VueRouter);
+
+let router = new VueRouter({
+  hashboang: false,
+  history: true
+});
+
+router.map({
+  '/': {
+      component: App
+  },
+  '/Map': {
+      component: Map
+  }
+});
+
+let main = Vue.extend({})
+
+router.start(main, 'body')
+
